cc3b4ec4dd8936bcdfc1f3e17705b5a12004a61d
James Bloxham <11bloxhamj@gmail.com>
2022-08-11 13:28:13 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-# Script for running simulaion tests
+# Script for running simulation tests
 
 from simulation import Simulation
 
@@ -11,7 +11,6 @@ MAP = 'configs/small-15x13-narrow.json'
 # 'configs/medium-27x13-narrow.json'
 # 'configs/large-51x25-narrow.json'
 
-print("4 agents")
 
 a = Simulation(config=MAP, algorithm='CA*', nagents=4, thread=False)
 a.run()
@@ -19,45 +18,8 @@ a.run()
 a = Simulation(config=MAP, algorithm='HCA*', nagents=4, thread=False)
 a.run()
 
-print("8 agents")
-
-a = Simulation(config=MAP, algorithm='CA*', nagents=8, thread=False)
+a = Simulation(config=MAP, algorithm='CA*', nagents=4, thread=False)
 a.run()
 
-a = Simulation(config=MAP, algorithm='HCA*', nagents=8, thread=False)
+a = Simulation(config=MAP, algorithm='HCA*', nagents=4, thread=False)
 a.run()
-
-print("12 agents")
-
-a = Simulation(config=MAP, algorithm='CA*', nagents=12, thread=False)
-a.run()
-
-a = Simulation(config=MAP, algorithm='HCA*', nagents=12, thread=False)
-a.run()
-
-
-# # Large Warehouse
-
-# print("16 agents")
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='CA*', nagents=16, thread=False)
-# a.run()
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='HCA*', nagents=16, thread=False)
-# a.run()
-
-# print("20 agents")
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='CA*', nagents=20, thread=False)
-# a.run()
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='HCA*', nagents=20, thread=False)
-# a.run()
-
-# print("24 agents")
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='CA*', nagents=24, thread=False)
-# a.run()
-
-# a = Simulation(config='configs/large-38x20.json', algorithm='HCA*', nagents=24, thread=False)
-# a.run()
